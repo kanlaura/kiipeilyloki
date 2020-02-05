3bb79b7c20ddcac955709ce1fec0fddaabd52f7c
kanlaura <59911290+kanlaura@users.noreply.github.com>
2020-02-05 14:28:56 +0200

    kohdelista aloitettu

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -5,57 +5,107 @@ const eNimi = document.getElementById('etsiNimi');
 //napit
 const poistaNappi = document.getElementById('poistaNappi');
 const etsiNappi = document.getElementById('etsi');
+const arvioiNappi = document.getElementById('arvioi');
 
 //kentät
 const pVastaus = document.getElementById('poistaVastaus');
+const arvostelut = document.getElementById('arvostelut');
 
 //eventlistenerit
 poistaNappi.addEventListener('click', poistaKohde);
 etsiNappi.addEventListener('click', etsiKohde);
+arvioiNappi.addEventListener('click', listaaArviot);
 
+//Kutsuu ajantasaista listaa kaikista kohteista
+listaaArviot()
 
+// Poistaa kohteen JSON-tiedostosta
 function poistaKohde() {
     const kohde = pNimi.value
     fetch(`http://localhost:3000/api/mestat/${kohde}`, {
         method: 'DELETE',
     })
-    .then(function (res) {
-        return res.json();
-    })
-    .then((json) => {
-        if (json == 'kohde poistettu') {
-            pVastaus.innerHTML = `${kohde} poistettu listalta`
-        } else {
-            pVastaus.innerHTML = `Kohdetta ei löytynyt`
-        }
-        return;
-    })
+        .then(function (res) {
+            return res.json();
+        })
+        .then((json) => {
+            if (json == 'kohde poistettu') {
+                pVastaus.innerHTML = `${kohde} poistettu toivelistalta`
+            } else {
+                pVastaus.innerHTML = `Kohdetta ei löytynyt`
+            }
+            return;
+        })
 };
 
+//Etsi kohteen kaikki tiedot klikkaamalla "Etsi kohde Arvioi kohde kohdassa"
 function etsiKohde() {
     const kohde = eNimi.value
     fetch(`http://localhost:3000/api/mestat/${kohde}`)
-    .then(function (res) {
-        return res.json();
-    })
-    .then((json) => {
-        if (json == `{'msg': 'Ei sellaista kohdetta!'}`) {
-            console.log(`Kohdetta ei löytynyt`);
-        } else {
-            console.log(json);
-        }
-        return;
-    })
+        .then(function (res) {
+            return res.json();
+        })
+        .then((json) => {
+            if (json == `{'msg': 'Ei sellaista kohdetta!'}`) {
+                console.log(`Kohdetta ei löytynyt`);
+            } else {
+                console.log(json);
+            }
+            return;
+        })
+
 }
 
-// document.getElementById("lisaa").addEventListener("click", AddMesta);
+//Listaa arvioidut kohteet kohtaan kaikki arvioidut kohteet
+function listaaArviot() {
+    fetch(`http://localhost:3000/api/mestat/`)
+        .then(function (res) {
+            return res.json();
+        })
+        .then((json) => {
+            arvostelut.innerHTML = '';
+            if (json == `{'msg': 'Ei sellaista kohdetta!'}`) {
+                console.log(`Error`);
+            } else {
+                console.log(json);
+                for (let i = 0; i < json.length; i++) {
+                    const paikka = json[i].paikka;
+                    const kuvaus = json[i].kuvaus;
+                    const arvio = json[i].arvio;
 
-// function AddMesta() {
+                    const uusiLI = document.createElement('li');
+                    const uusiUL = document.createElement('ul');
+                    const uusiKuvaus = document.createElement('li');
+                    const uusiArvio = document.createElement('li');
 
-//     fetch("http:/localhost/3000/api/mesta").then(res => res.json())
-//     .then(paikat => {
-//         console.dir(paikat);
-//     })
+                    uusiLI.innerHTML = `${paikka}`
+                    uusiKuvaus.innerHTML = `${kuvaus}`;
+                    uusiArvio.innerHTML = `${arvio}`;
+
+                    arvostelut.appendChild(uusiLI);
+                    uusiLI.appendChild(uusiUL);
+                    uusiUL.appendChild(uusiKuvaus);
+                    uusiUL.appendChild(uusiArvio);
+
+                    uusiLI.classList.add('aKohde');
+                    uusiUL.classList.add('aLista');
+                    uusiKuvaus.classList.add('aKuvaus');
+                    uusiArvio.classList.add('aArvio');
+                }
+            }
+            return;
+        })
+}
 
 
-// }
+// document.querySelectorAll('.aKohde').forEach(function (item) {
+//     item.addEventListener('click', function (event) {
+//         console.log('jee jee')
+//         const kuvaus = document.getElementsByClassName('aLista');
+//         if (kuvaus.style.display !== 'none') {
+//             kuvaus.style.display = 'none';
+//         } else {
+//             kuvaus.style.display = 'inline';
+//         }
+//     });
+// });
--- routes/mestat.js
+++ routes/mestat.js
@@ -5,53 +5,23 @@ var paikat = require("../paikat.json") //tämä johtaa GETin json-tiedostoon (H.
 
 
 //tuo näkyviin koko lista (H.V. & D.B.)
-router.get('/', function (req, res) {
-    res.send(paikat);
-});
-
-
-router.route('/:paikka')
-    .put(function (req, res) {
-        console.log("Put: " + req.params.paikka)
-        console.dir(req.body)
-        console.dir(paikat)
-        for (let item of paikat) {
-            if (item.paikka === req.params.paikka) {
-                const change = req.body;
-                if (change.arvio) {
-                    item.arvio = change.arvio
-                    console.log(item.arvio);
-                }
-                if (change.kuvaus) {
-                    item.kuvaus = change.kuvaus
-                    console.log(item.kuvaus);
-                }
-                fs.writeFile("paikat.json", JSON.stringify(paikat), (err) => {
-                    if (err) throw err;
-                    res.json({ Viesti: 'Muutettu' });
-                    return;
-                })
-            }
-        }
-        res.status(404);
-        res.json("{'msg': 'Tämän nimistä paikkaa ei löytynyt!'}");
-    });
-
-
-//lisää uusi kohde (H.V. & D.B.)
-router.post("/", function (req, res) {
-    let uusi = req.body;
-    paikat.push(uusi);
-    fs.writeFile("paikat.json", JSON.stringify(paikat), (err) => {
-        if (err) throw err;
-        res.end(respdata);
+router.route('/')
+    .get(function (req, res) {
+        res.send(paikat);
     })
-    res.status(201).json(uusi);
-})
 
-//hae paikannimellä (H.V. & D.B.)
+    //lisää uusi kohde (H.V. & D.B.)
+    .post(function (req, res) {
+        let uusi = req.body;
+        paikat.push(uusi);
+        fs.writeFile("paikat.json", JSON.stringify(paikat), (err) => {
+            if (err) throw err;
+            res.end(respdata);
+        })
+        res.status(201).json(uusi);
+    })
+
 router.route('/:paikka')
-
     .get(function (req, res) {
         for (var kohde of paikat) {
             if (kohde.paikka == req.params.paikka) {
@@ -60,6 +30,31 @@ router.route('/:paikka')
             }
         }
         res.json("{'msg': 'Ei sellaista kohdetta!'}"); //jos haetun nimistä paikkaa ei löydy (H.V. & D.B.)
+    })
+
+    .put(function (req, res) {
+        // console.log("Put: " + req.params.paikka)
+        // console.dir(req.body)
+        // console.dir(paikat)
+        for (let item of paikat) {
+            if (item.paikka === req.params.paikka) {
+                const change = req.body;
+                if (change.arvio) {
+                    item.arvio = change.arvio
+                    // console.log(item.arvio);
+                }
+                if (change.kuvaus) {
+                    item.kuvaus = change.kuvaus
+                    // console.log(item.kuvaus);
+                }
+                fs.writeFile("paikat.json", JSON.stringify(paikat), (err) => { })
+                res.json('Muutettu');
+                return;
+
+            }
+        }
+        res.status(404);
+        res.json("{'msg': 'Tämän nimistä paikkaa ei löytynyt!'}");
     })
 
     .delete(function (req, res, next) {
@@ -74,7 +69,9 @@ router.route('/:paikka')
             }
         }
         res.json('kohdetta ei löytynyt');
-    })
+    });
+
+
 
 module.exports = router;
 

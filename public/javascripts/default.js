d811a2e33736505818e485a1587a40292dfc6de8
Hannamaa <hanna.m.valli@gmail.com>
2020-02-05 15:29:33 +0200

    lisätty Toivelistan POST ja GET fronttiin

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -1,12 +1,53 @@
-//käyttäjä lisää selaimessa uuden kohteen listalle.
-//Tämä scripti lisää sen POStilla paikat.jsoniin (H.V. ja D.B)
+document.getElementById("lisaa").addEventListener("click", addMesta);
 
-document.getElementById("uusiNimi").addEventListener("click", munFunktio);
-
-function munFunktio() {
-    fetch('..paikat.json', post)
-
+//Toivelista näkyy kun käyttäjä tulee sivulle (H.V. ja D.B)
+function myFunction(){
+    fetch("./api/mestat") 
+    .then(res => res.json())
+    .then(paikat => {
+        //console.log(paikat)
+        let output=""
+        for (i=0; i<paikat.length; i++) {
+            if (paikat[i].arvio == ""){
+                console.log(paikat[i].paikka)
+                output += `<li>${paikat[i].paikka}</li>`
+            }
+        ;
+        //console.log(paikat[i].paikka)
+        }
+        document.getElementById("output").innerHTML = output;
+    })
 }
 
+myFunction();
 
-//id toivelista - näyttää käyttäjän listaamat toivekohteet (H.V. ja D.B)
+//käyttäjä lisää selaimessa uuden kohteen listalle.
+//Tämä scripti lisää sen POStilla paikat.jsoniin (H.V. ja D.B)
+function addMesta() {
+    let title = document.getElementById("uusiNimi").value
+    fetch("./api/mestat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ paikka: title, kuvaus: "", arvio: "" })
+    })
+        .then(res => res.json())
+        
+
+     //tämä on GET pyyntö palauttaa lisäyksen jälkeen näkyviin toivelistalle paikat, 
+     //jolla ei ole arviota (H.V. ja D.B)
+    fetch("./api/mestat") 
+    .then(res => res.json())
+    .then(paikat => {
+        //console.log(paikat)
+        let output=""
+        for (i=0; i<paikat.length; i++) {
+            if (paikat[i].arvio == ""){
+                console.log(paikat[i].paikka)
+                output += `<li>${paikat[i].paikka}</li>`
+            }
+        ;
+        //console.log(paikat[i].paikka)
+        }
+        document.getElementById("output").innerHTML = output;
+    })
+    }
